9872368a5208e31dfeb2bc11095ee980f03b861b
DarioFuentealba <117101633+DarioFuentealba@users.noreply.github.com>
2022-11-04 01:59:01 -0300

    Update and rename parte3_ejer_4-5-6.php to numeroEntre_min_max.php
    Solución al módulo donde hay que encontrar un número entre ($min, $max)
    
    Usé ctype_digit, me pareció correcto porque en otro módulo se usa ctype_alpha
    ctype_digit --> Chequear posibles caracteres numéricos
    is_int --> Comprueba si el tipo de una variable es integer
    Viendo lo que significa is_int, pensé que eso sólo podía verificar el tipo (INT, FLOAT, BOOLEAN), pero no el valor real de la variable, ahí recorde que teníamos una que chequeaba que la variable fuera sólo de string, entonces tenía que haber una que hiciera lo mismo pero con números enteros.

---
+++ numeroEntre_min_max.php
@@ -0,0 +1,74 @@
+<?php
+
+
+//MUESTRO ORIGINAL Y NUEVA PARA QUE VEAN DIFERENCIAS
+
+/*PROBE INGRESANDO LOS SIGUENTES DATOS:
+    1) Nro < $min
+    2) Nro > $max
+    3) 16.5
+    4) a) a
+       b) A
+       c)aetsgdhhf
+    5) 16gdsfdgfhgkj
+    6) 16
+    
+EL UNICO QUE FUNCIONÓ FUE EL PUNTO 6) DONDE INGRESE 16
+*/
+
+
+
+/*
++++++++++++++++++++++++++++++++++++++++
+ESTA ES LA ORIGINAL
++++++++++++++++++++++++++++++++++++++++
+*/  
+    /** Pide al usuario un número entre un rango de valores. Si éste no es válido, se pide de nuevo
+ * @param INT $min
+ * @param INT $max
+ * @return INT Retorna un número válido
+ */
+function solicitarNumeroEntre($min, $max)
+{
+    //int $numero
+    $numero = trim(fgets(STDIN));
+    while (!is_int($numero) && !($numero >= $min && $numero <= $max)) {
+        echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
+        $numero = trim(fgets(STDIN));
+    }
+    return $numero;
+}
+
+
+
+
+
+
+/*
++++++++++++++++++++++++++++++++++++++++
+ESTA ES LA QUE HICE
++++++++++++++++++++++++++++++++++++++++
+*/
+
+/** Pide al usuario un número entre un rango de valores. Si éste no es válido, se pide de nuevo
+ * @param INT $min
+ * @param INT $max
+ * @return INT Retorna un número válido
+ */
+function solicitarNumeroEntre($min, $max)
+{
+    echo "ingrese un numero: ";
+    $numero = trim(fgets(STDIN));
+    echo "numero: ".$numero."\n";
+    echo "\n";
+
+        while (!ctype_digit($numero) || $numero < $min || $numero > $max) {
+            echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
+            $numero = trim(fgets(STDIN));
+            echo "\n";
+            echo "El numero es: ".$numero."\n";
+        }
+    echo "El numero final es: ".$numero."\n";
+
+    return $numero;
+}
